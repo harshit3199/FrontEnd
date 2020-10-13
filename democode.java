fa8d2c27b4f89888cbebca8ced21f02d6a6aa29d
harshit3199 <57800413+harshit3199@users.noreply.github.com>
2020-10-13 22:53:34 +0530

    Update democode.java

--- democode.java
+++ democode.java
@@ -1,6 +1,6 @@
 class hacktoberfest
 {
   public static void main(String args[]){
-    System.out.println("This is the hacktober fest pull request");
+    System.out.println("This is the hacktober fest request");
   }
 }
